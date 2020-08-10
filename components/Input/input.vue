02b4cb6ddb6b892311646891d7f8b6e3600eb9a0
wangqiang8 <wangqiang8@kingsoft.com>
2020-08-10 18:36:16 +0800

    feat(input): 组件developing

--- components/Input/input.vue
+++ components/Input/input.vue
@@ -7,21 +7,24 @@
                     'kd-input-disabled': disabled,
                     'kd-input-group': $slots.prepend || $slots.append,
                     'kd-with-prefix': $slots.prefix,
-                    'kd-with-suffix': $slots.suffix
+                    'kd-with-suffix': $slots.suffix || success || warning || error,
+                    'kd-input-fluid': fluid,
+                    ['kd-input-status-' + (status === 'error' ? 'err' : status)]: success || warning || error,
+                    'kd-textarea-show-count': showCount
                 }
             ]"
-            :style="{width: `${width}px`}"
+            :style="{width: parseInt(width) + 'px'}"
     >
         <div
                 v-if="$slots.prepend"
-                class="kd-prepend"
+                class="kd-input-prepend"
         >
             <slot name="prepend"></slot>
         </div>
-        <div class="kd-wrapper">
+        <div class="kd-input-wrapper">
             <div
                     v-if="$slots.prefix"
-                    class="kd-prefix"
+                    class="kd-input-prefix"
             >
                 <slot name="prefix"></slot>
             </div>
@@ -30,40 +33,67 @@
                     ref="input"
                     v-model="innerValue"
                     v-bind="$attrs"
-                    class="kd-inner"
+                    class="kd-input-inner"
                     :type="type"
                     :readonly="readonly"
                     :disabled="disabled"
+                    :tabindex="tabindex"
                     @focus="focusHandler"
                     @change="changeHandler"
-                    @compositionstart="compositionStartHandler"
-                    @compositionend="compositionEndHandler"
                     @blur="blurHandler"
+                    @keydown="keydownHandler"
+                    @keyup="keyupHandler"
             />
             <textarea
                     v-else
                     ref="textarea"
                     v-model="innerValue"
-                    class="kd-inner kd-textarea"
-                    :type="type"
                     v-bind="$attrs"
+                    class="kd-input-inner"
+                    :type="type"
                     :readonly="readonly"
                     :disabled="disabled"
+                    :rows="rows"
                     :style="textareaStyle"
+                    :tabindex="tabindex"
                     @focus="focusHandler"
                     @change="changeHandler"
                     @blur="blurHandler"
+                    @keydown="keydownHandler"
+                    @keyup="keyupHandler"
             ></textarea>
             <div
-                    v-if="$slots.suffix"
-                    class="kd-suffix"
+                    v-if="$slots.suffix || clearable || success || warning || error"
+                    class="kd-input-suffix"
             >
+                <i
+                        v-if="success || warning || error"
+                        :class="{
+                            'kd-icon-success-solid': success,
+                            'kd-icon-warning-solid': warning,
+                            'kd-icon-error-solid': error
+                        }"
+                >
+                </i>
+                <i
+                        v-else-if="showClearable"
+                        class="kd-icon-error-solid kd-input-clearable"
+                        :class="{'kd-input-show': value}"
+                        @click="clear"
+                >
+                </i>
                 <slot name="suffix"></slot>
             </div>
+            <span
+                    v-if="isShowCount"
+                    class="kd-textarea-count"
+            >
+                {{ textLength }}/{{ limit }}
+            </span>
         </div>
         <div
                 v-if="$slots.append"
-                class="kd-append"
+                class="kd-input-append"
         >
             <slot name="append"></slot>
         </div>
@@ -75,9 +105,11 @@
         name: 'Input',
         inheritAttrs: false,
         props: {
+            tabindex: {
+                type: String
+            },
             value: {
-                type: [Number, String],
-                default: ''
+                type: [Number, String]
             },
             width: {
                 type: [Number, String]
@@ -99,29 +131,61 @@
                 default: 'default'
             },
             status: {
-                type: String,
-                default: ''
+                type: String
+            },
+            clearable: {
+                type: Boolean,
+                default: false
+            },
+            fluid: {
+                type: Boolean,
+                default: false
+            },
+            resize: {
+                type: String
+            },
+            rows: {
+                type: [Number, String],
+                default: 3
+            },
+            showCount: {
+                type: Boolean,
+                default: false
             }
         },
         data() {
             return {
-                classObj: {
-                    'kd-group': this.$slots.prepend || this.$slots.append,
-                    'kd-disabled': this.disabled,
-                    'kd-input-default': this.size === 'default',
-                    'kd-mini-input': this.size === 'mini',
-                    'kd-small-input': this.size === 'small',
-                    'kd-large-input': this.size === 'large',
-                    'kd-input-success': this.stau
-                },
                 innerValue: '',
-                isComposing: false,
                 currentTextareaStyle: {}
             };
         },
         computed: {
             textareaStyle() {
-                return Object.assign({}, this.currentTextareaStyle, {resize: this.resize});
+                return Object.assign({}, this.currentTextareaStyle, {resize: this.resize, width: parseInt(this.width) + 'px'});
+            },
+            success() {
+                return this.status === 'success';
+            },
+            error() {
+                return this.status === 'error';
+            },
+            warning() {
+                return this.status === 'warning';
+            },
+            showClearable: function () {
+                return this.clearable && !this.readonly && !this.disabled && this.innerValue && (this.type !== 'textarea');
+            },
+            isShowCount() {
+                return this.showCount && this.$attrs.maxlength && this.type === 'textarea' && !this.disabled && !this.readonly;
+            },
+            textLength() {
+                if (typeof this.value === 'number') {
+                    return String(this.value).length;
+                }
+                return (this.value || '').length;
+            },
+            limit() {
+                return this.$attrs.maxlength;
             }
         },
         watch: {
@@ -129,8 +193,7 @@
                 if (v === this.value) return;
                 this.$emit('input', v);
             },
-            value(v, v2) {
-                console.log(v, v2);
+            value(v) {
                 this.innerValue = v;
                 this.$emit('change', v);
             }
@@ -139,22 +202,38 @@
             this.innerValue = this.value;
         },
         methods: {
+            getInputElem() {
+                return this.$refs.input || this.$refs.textarea;
+            },
+            focus() {
+                this.getInputElem().focus();
+            },
+            blur() {
+                this.getInputElem().blur();
+            },
+            select() {
+                this.getInputElem().select();
+            },
             focusHandler(event) {
                 this.$emit('focus', event);
-            },
-            compositionStartHandler() {
-                this.isComposing = true;
-            },
-            compositionEndHandler() {
-                this.isComposing = true;
             },
             changeHandler(event) {
                 this.$emit('change', event.target.value);
             },
             blurHandler(event) {
                 this.$emit('blur', event);
+            },
+            clear() {
+                this.$emit('input', '');
+                this.$emit('change', '');
+                this.$emit('clear');
+            },
+            keydownHandler(e) {
+                this.$emit('keydown', e);
+            },
+            keyupHandler(e) {
+                this.$emit('keyup', e);
             }
-
         }
     };
 </script>
