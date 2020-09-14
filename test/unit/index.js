5b85ab7f88591d8fab1ea4ffdec11fe99b501e60
wangqiang8 <wangqiang8@kingsoft.com>
2020-09-14 15:56:09 +0800

    test(input): test merge

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-const testsContext = require.context('./specs', true, /input\.spec$/);
-=======
 const testsContext = require.context('./specs', true, /.spec$/);
->>>>>>> dev
 
 testsContext.keys().forEach(testsContext);
