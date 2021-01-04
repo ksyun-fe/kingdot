603414c348b57b62d643692520b035b42db3c41e
groot <2569679906@qq.com>
2021-01-04 21:30:51 +0800

    build: example webpack prof

--- build/webpack.demo.conf.js
+++ build/webpack.demo.conf.js
@@ -180,8 +180,7 @@ if (isProd) {
         cacheGroups: {
             vendors: {
                 test: /[\\/]node_modules[\\/]/,
-                priority: -10,
-                // filename: 'vendors.[contenthash].js'
+                priority: -10
             },
             common: {
                 minChunks: 2,
