bb485d726f1bb3e3952bf48d3bee7de6f51bcc8b
rendaxue <w_rendaxue@kingsoft.com>
2021-02-20 14:59:07 +0800

    fix: 修改spinner样式

---
+++ components/Spinner/spinner.vue
@@ -0,0 +1,215 @@
+<template>
+    <div
+            :class="[
+                'kd-spinner-' + size,
+                {
+                    'kd-spinner-right': buttonPosition,
+                    'kd-spinner-disabled': disabled,
+                }
+            ]"
+            class="kd-spinner"
+    >
+        <i
+                :class="minusStyles"
+                class="kd-spinner-btn"
+                @click="minusNum"
+        ></i>
+        <i
+                :class="addStyles"
+                class="kdicon kd-spinner-btn"
+                @click="addNum"
+        ></i>
+        <kd-input
+                ref="input"
+                v-model="inputValue"
+                :class="[`kd-spinner-${buttonPosition?buttonPosition +'-': ''}input`]"
+                :disabled="disabled"
+                class="kd-spinner-input"
+                :placeholder="placeholder"
+                :size="size || 'default'"
+                @input="handleChange"
+                @blur="handleBlur"
+                @focus="handleFocus"
+                @keydown.up="addNum"
+                @keydown.down="minusNum"
+        />
+    </div>
+</template>
+<script>
+    import KdInput from '../Input/index';
+    export default {
+        name: 'KdSpinner',
+        components: {
+            KdInput
+        },
+        props: {
+            value: {
+                type: Number,
+                default: 0
+            },
+            min: {
+                type: Number,
+                default: null
+            },
+            max: {
+                type: Number,
+                default: null
+            },
+            step: {
+                type: Number,
+                default: null
+            },
+            precision: {
+                type: Number,
+                default: null
+            },
+            buttonPosition: {
+                type: String,
+                default: ''
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            placeholder: {
+                type: String,
+                default: ''
+            },
+            size: {
+                type: String,
+                default: 'default'
+            },
+            inputStep: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+                inputValue: null,
+                isMax: false,
+                isMin: false
+            };
+        },
+        computed: {
+            addStyles() {
+                const pos = this.buttonPosition;
+                const disabled = this.disabled;
+                const isMax = this.isMax;
+                const styles = [
+                    `kd-spinner-${pos ? pos + '-' : ''}add`,
+                    pos ? 'kd-icon-arrow-up-solid' : 'kd-icon-plus-solid',
+                    {
+                        'kd-spinner-disabled-btn': disabled || isMax
+                    }
+                ];
+                return styles;
+            },
+            minusStyles() {
+                const pos = this.buttonPosition;
+                const disabled = this.disabled;
+                const isMin = this.isMin;
+                const styles = [
+                    `kd-spinner-${pos ? pos + '-' : ''}minus`,
+                    pos ? 'kd-icon-arrow-down' : 'kd-icon-minus-solid',
+                    {
+                        'kd-spinner-disabled-btn': disabled || isMin
+                    }
+                ];
+                return styles;
+            }
+        },
+        watch: {
+            value: {
+                immediate: true,
+                handler(v) {
+                    let value = v;
+                    //  小数点
+                    if (this.precision !== null) {
+                        value = this.addPoint(value);
+                    }
+                    this.isMax = v === this.max;
+                    this.isMin = v === this.min;
+                    console.log(value);
+                    this.inputValue = value;
+                }
+            }
+        },
+        methods: {
+            // 增加
+            addNum() {
+                console.log(this.inputValue);
+                let value = Number(this.inputValue);
+                const max = this.max;
+                if (value === this.max || this.disabled) return;
+                if (this.step === null) {
+                    value += 1;
+                } else {
+                    value = (value * 10 + this.step * 10) / 10;
+                }
+                if (value > max) {
+                    value = max;
+                }
+                console.log(value);
+                this.$emit('input', value);
+                this.$emit('change', value, this.value);
+            },
+            //  减小
+            minusNum() {
+                console.log(this.inputValue);
+                let value = Number(this.inputValue);
+                console.log(value);
+                const min = this.min;
+                if (value === this.min || this.disabled) return;
+                if (this.step === null) {
+                    value -= 1;
+                } else {
+                    value -= this.step;
+                }
+                if (value < min && min !== null) {
+                    value = min;
+                }
+                this.$emit('input', value);
+                this.$emit('change', value, this.value);
+            },
+            handleChange(v) {
+                const newv = Number(v);
+                if (v === '' || isNaN(newv)) return;
+                // 小数点转换number,会导致值为整数
+                this.inputValue = v;
+                this.$emit('change', newv, this.value);
+            },
+            handleBlur(e) {
+                const step = this.step;
+                let value = Number(this.inputValue);
+                // 输入精准
+                if (this.inputStep && step !== null) {
+                    const num = value / step;
+                    // 四舍五入取两位小数
+                    const pow = Math.pow(10, this.precision);
+                    value = Math.floor(Math.round(num) * pow) / pow * step;
+                }
+                if (this.precision !== null) {
+                    this.inputValue = this.addPoint(value);
+                }
+                console.log(this.inputValue);
+                this.$emit('blur', e);
+            },
+            addPoint(value) {
+                return value.toFixed(this.precision);
+            },
+            handleFocus(e) {
+                this.$emit('focus', e);
+            },
+            focus() {
+                this.$refs.input.focus();
+            },
+            blur() {
+                this.$refs.input.blur();
+            },
+            select() {
+                this.$refs.input.select();
+            }
+        }
+    };
+</script>
---
+++ examples/docs/zh-CN/spinner.md
@@ -0,0 +1,213 @@
+## spinner 计数器
+通过鼠标或者键盘，定义范围内的数值。
+
+### 基本使用
+:::demo #计数器 ##计数器的基本使用
+
+```html
+<template>
+    <kd-spinner v-model="value" :min="0" :max="10" @change="change"></kd-spinner>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            value: 0
+        }
+    },
+    methods:{
+        change(v,oldv){
+            console.log(v, oldv)
+        }
+    }
+}
+</script>
+```
+
+:::
+
+### 禁用
+:::demo #计数器 ##禁用状态的计数器
+
+```html
+<template>
+    <kd-spinner v-model="value" :min="0" :max="10" disabled></kd-spinner>
+
+</template>
+<script>
+export default{
+    data(){
+        return{
+            value: 1
+        }
+    },
+}
+</script>
+```
+
+:::
+
+### 小数点
+:::demo #计数器 ##precision是设置小数点几位。必须是个非负数。带有小数点的计数器
+
+```html
+<template>
+    <kd-spinner v-model="value" :min="-10" :max="10" :step="5.4" :precision="2"></kd-spinner>
+
+</template>
+<script>
+export default{
+    data(){
+        return{
+            value: 1
+        }
+    },
+}
+</script>
+```
+
+:::
+
+### 步数
+:::demo #计数器 ## 带有步数的计数器。
+
+```html
+<template>
+    <kd-spinner v-model="value" :min="0" :max="10" :step="0.2"></kd-spinner>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            value: 0.1
+        }
+    },
+}
+</script>
+```
+
+:::
+
+### 按钮位置
+:::demo #计数器 ##不同的按钮位置展示。
+
+```html
+<template>
+    <div class="row">
+        <kd-spinner v-model="value" :min="0" :max="10" :step="2"></kd-spinner>
+        <kd-spinner v-model="value" button-position="right" :min="0" :max="10" :step="2"></kd-spinner>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            value: 1
+        }
+    },
+}
+</script>
+```
+
+:::
+
+### 大小
+:::demo #计数器 ##不同大小的按钮展示
+
+```html
+<template>
+    <div>
+        <div class="row">
+            <kd-spinner v-model="value" button-position="right" size="large"></kd-spinner>
+            <kd-spinner v-model="value" button-position="right" ></kd-spinner>
+            <kd-spinner v-model="value" button-position="right" size="small"></kd-spinner>
+            <kd-spinner v-model="value" button-position="right" size="mini"></kd-spinner>
+        </div>
+        <div class="row comp-top">
+            <kd-spinner v-model="value" size="large"></kd-spinner>
+            <kd-spinner v-model="value" ></kd-spinner>
+            <kd-spinner v-model="value" size="small"></kd-spinner>
+            <kd-spinner v-model="value" size="mini"></kd-spinner>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            value: 1
+        }
+    },
+}
+</script>
+<style>
+.row .kd-spinner{
+    margin-left: 10px;
+}
+</style>
+```
+
+:::
+
+### 精准步数
+:::demo #计数器 ##在输入数字的时候，严格按照步数。
+
+```html
+<template>
+    <kd-spinner ref="spinner" v-model="value" input-step :min="0" :max="10" :step="0.2" :precision="2" @change="change"></kd-spinner>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            value: 1
+        }
+    },
+    mounted(){
+        // this.$refs.spinner.focus();
+        // this.$refs.spinner.select();
+    },
+    methods:{
+        change(v,oldv){
+            console.log(v,oldv)
+            if(v < 5 ){
+                if(v > oldv){
+                    this.value = 5
+                }else{
+                    this.value = 0
+                }
+            }
+        }
+    }
+}
+</script>
+```
+:::
+
+### 属性 {.component__content}
+| 属性      | 说明    | 类型      | 可选值       | 默认值   |
+|---------- |-------- |---------- |-------------  |-------- |
+| value/v-model | 更新值 | number  | — | — |
+| min | 最小值 | number  | — | — |
+| max | 最大值 | number  | — | — |
+| step | 步数 | number  | — | — |
+| precision | 小数点个数 | number  | — | — |
+| buttonPosition | 按钮位置 | string  | — | — |
+| disabled | 禁用 | boolean  | — | false |
+| placeholder | 提示文字 | string  | — | — |
+| size | 大小 | string  | — | large / small / mini |
+| inputStep | 精准严格步数 | boolean | — | false |
+
+### 事件 {.component__content}
+| 属性      | 说明    | 回调参数 |
+|---------- |-------- |-------- |
+| change | 值改变时触发 | currentValue,oldValue
+| focus | input获取焦点时触发 | event
+| blur | input失去焦点时触发 | event
+
+### 方法 {.component__content}
+| 属性      | 说明    | 参数 |
+|---------- |-------- |-------- |
+| focus | input获得焦点 | —
+| select | input内的文字全选 | —
