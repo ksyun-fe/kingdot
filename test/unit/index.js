b27efca79b09500507010d2686cce04ef77256eb
rendaxue <w_rendaxue@kingsoft.com>
2021-04-02 15:57:25 +0800

    fix: changelog

--- test/unit/index.js
+++ test/unit/index.js
@@ -15,7 +15,7 @@ describe('use component', () => {
 });
 Vue.use(kingdot);
 
-const testsContext = require.context('./specs', true, /\.spec$/);
+const testsContext = require.context('./spec', true, /\button.spec$/);
 
 
 testsContext.keys().forEach(testsContext);
