bb8324bed9e6d9c815c508820fa28d4299913c0b
Joey <w_18@163.com>
2021-05-11 03:03:35 +0800

    feat(datetimepicker): 选日期后跳到时间页面 样式调整

--- components/DateTimePicker/date-time-picker.vue
+++ components/DateTimePicker/date-time-picker.vue
@@ -67,32 +67,32 @@
                         <!-- :selected-date="inputDateString" -->
                         <div
                                 v-show="tabSelectValue === 'date'"
-                                class="kd-date-picker"
+                                class="kd-picker-wrapper"
                         >
                             <Calendar
                                     ref="calendar"
-                                    v-model="dateValue"
+                                    v-model="dateTimeValue"
                                     :format-string="formatString"
                                     :disabled-date="disabledDate"
                                     :max-date="max"
                                     :min-date="min"
                                     @pageChange="pageChange"
-                                    @select="popDateValue"
+                                    @select="popdateTimeValue"
                             ></Calendar>
                         </div>
                         <div
                                 v-show="tabSelectValue === 'time'"
-                                class="kd-date-picker"
+                                class="kd-picker-wrapper"
                         >
                             <Time
                                     v-model="tabTimeString"
-                                    :count="7"
+                                    :count="9"
                             >
                             </Time>
                             <!-- @change="timeValueChange" -->
                         </div>
                         <div class="kd-datetime-footer">
-                            <kd-button @click="isTooltipShow=false">确认</kd-button>
+                            <kd-button @click="confirmDateTime">确认</kd-button>
                             <kd-button @click="isTooltipShow=false">关闭</kd-button>
                         </div>
                     </div>
@@ -118,7 +118,7 @@
                     return [];
                 }
             },
-            // dateValue: {
+            // dateTimeValue: {
             //     type: Array,
             //     default: function () {
             //         return [];
@@ -177,7 +177,7 @@
         },
         data() {
             return {
-                dateValue: [],
+                dateTimeValue: [],
                 inputDateString: '',
                 max: this.maxDate ? Moment(this.maxDate).format(this.formatString) : '',
                 min: this.minDate ? Moment(this.minDate).format(this.formatString) : '',
@@ -198,12 +198,12 @@
                             v = '';
                         }
                         this.inputDateString = v;
-                        this.dateValue = [v];
+                        this.dateTimeValue = [v];
                     } else if (Array.isArray(v)) {
                         const tmpArr = v.filter(timeStr => Moment(timeStr).isValid());
                         v = tmpArr.length > 2 ? tmpArr.slice(0, 2) : tmpArr;
                         this.inputDateString = v.join(' ~ ');
-                        this.dateValue = v;
+                        this.dateTimeValue = v;
                     }
                 }
             },
@@ -221,8 +221,16 @@
             }
         },
         methods: {
+            confirmDateTime() {
+                this.isTooltipShow = false;
+                this.tabSelectValue = 'date';
+                // dateTimeValue 存时间戳
+                // this.$emit('input', dateArr[0]);
+                this.inputDateString = `${this.tabDateString} ${this.tabTimeString}`;
+                // this.inputDateString = `${this.tabDateString} ${this.tabTimeString}`;
+            },
             onDayMouseenter(date) {
-                // const [begin, end] = this.dateValue;
+                // const [begin, end] = this.dateTimeValue;
                 // const isSetRange = this.range && begin && !end;
                 this.rangeEndDate = this.range ? date : '';
             },
@@ -241,26 +249,27 @@
                 }
             },
 
-            // 底下上传上来的数据  在这里不给dateValue赋值. 从底下来的数据, 又通过props传下去不合理
+            // 底下上传上来的数据  在这里不给dateTimeValue赋值. 从底下来的数据, 又通过props传下去不合理
             // 应该用 watch 监听value, 然后改变 inputDateString. 就没有select 的事情了 吗??
-            popDateValue(dateArr, source = 'calendar') {
-                if (this.range && dateArr.length < 2) return;
-                if (this.range) {
-                    this.$emit('input', dateArr);
-                    this.inputDateString = dateArr.join(' ~ ');
-                } else {
-                    this.$emit('input', dateArr[0]);
-                    this.inputDateString = dateArr[0];
-                    this.tabDateString = dateArr[0];
-                }
-                // console.log('emit change', this.inputDateString, source);
-                this.dateValue = dateArr;
-                this.$emit('change', this.dateValue, source);
-                if (source !== 'shortcuts') {
-                    // this.isTooltipShow = false;
-                    // 切到 time 页面
-                    this.tabSelectValue = 'time';
-                }
+            // 只是日期改变
+            popdateTimeValue(dateArr, source = 'calendar') {
+                this.tabSelectValue = 'time';
+                this.tabDateString = dateArr[0];
+                // if (this.range && dateArr.length < 2) return;
+                // if (this.range) {
+                //     this.$emit('input', dateArr);
+                //     this.inputDateString = dateArr.join(' ~ ');
+                // } else {
+                //     this.$emit('input', dateArr[0]);
+                //     this.inputDateString = dateArr[0];
+                //     this.tabDateString = dateArr[0];
+                // }
+                // this.dateTimeValue = dateArr;
+                // if (source !== 'shortcuts') {
+                //     // this.isTooltipShow = false;
+                //     // 切到 time 页面
+                //     this.tabSelectValue = 'time';
+                // }
             },
             // input 回车和blur事件触发
             setDate(inputValue) {
@@ -287,29 +296,29 @@
                     const aimDateStr = Moment().add(value, unit).format(this.formatString);
                     if (value > 0) {
                         // TODO: 快捷选项的时间 不一定是以当前时间为起止点.
-                        this.dateValue.splice(0, this.dateValue.length, Moment().format(this.formatString), aimDateStr);
+                        this.dateTimeValue.splice(0, this.dateTimeValue.length, Moment().format(this.formatString), aimDateStr);
                     } else {
-                        this.dateValue.splice(0, this.dateValue.length, aimDateStr, Moment().format(this.formatString));
+                        this.dateTimeValue.splice(0, this.dateTimeValue.length, aimDateStr, Moment().format(this.formatString));
                     }
-                    this.inputDateString = this.dateValue.join(' ~ ');
+                    this.inputDateString = this.dateTimeValue.join(' ~ ');
 
                     // 调整两页的日历渲染月份
-                    this.$refs.startCalendar.jumpToDate(this.dateValue, 'shortcuts');
-                    this.$refs.endCalendar.jumpToDate(this.dateValue, 'shortcuts');
+                    this.$refs.startCalendar.jumpToDate(this.dateTimeValue, 'shortcuts');
+                    this.$refs.endCalendar.jumpToDate(this.dateTimeValue, 'shortcuts');
                 } else {
                     // 时间点模式
                     const aimDateStr = Moment().add(value, unit).format(this.formatString);
-                    this.dateValue.splice(0, this.dateValue.length, aimDateStr);
+                    this.dateTimeValue.splice(0, this.dateTimeValue.length, aimDateStr);
                     this.$refs.calendar.jumpToDate(aimDateStr, 'shortcuts');
                 }
             },
             onClear(e) {
                 e.stopPropagation();
-                this.dateValue = [];
+                this.dateTimeValue = [];
                 this.inputDateString = '';
                 // this.emitChange();
                 this.$emit('clear', '');
-                this.$emit('change', this.dateValue, 'clear');
+                this.$emit('change', this.dateTimeValue, 'clear');
                 if (this.range) {
                     this.$emit('input', []);
                 } else {
