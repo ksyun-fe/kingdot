73f789b24bd3e55b7bfed29ce8f5da2031d4c485
zhaojiale <zhaojiale@kingsoft.com>
2021-11-10 15:33:43 +0800

    feat(skeleton): new component

---
+++ components/Skeleton/index.js
@@ -0,0 +1,7 @@
+import Skeleton from './skeleton.vue';
+
+Skeleton.install = (Vue) => {
+    Vue.component(Skeleton.name, Skeleton);
+};
+
+export default Skeleton;
---
+++ components/Skeleton/skeleton.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+    <!-- 圆形 -->
+
+    <!-- 圆形组 -->
+
+    <!-- 文本段落 -->
+
+    <!-- 方形 -->
+
+    <!-- 表格 -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'KdSkeleton',
+        props: {
+            // 是否开启动画
+            active: {
+                type: Boolean,
+                default() {
+                    return false;
+                }
+            },
+            // 是否显示骨架层
+            value: {
+                type: Boolean,
+                default() {
+                    return true;
+                }
+            }
+        },
+        data() {
+            return {};
+        },
+        computed: {},
+        watch: {},
+        methods: {}
+    };
+</script>
