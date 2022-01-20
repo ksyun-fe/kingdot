0c0d792af6ac80fe2191c48c874b636060e86195
赵佳乐 <zhaojiale@dev.ksyun.com>
2022-01-20 16:50:54 +0800

    fix: drawer测试用例更改

--- test/unit/specs/drawer.spec.js
+++ test/unit/specs/drawer.spec.js
@@ -12,7 +12,6 @@ describe('drawer', () => {
             value: true
         })
         expect(vm.$el.classList.contains('kd-drawer')).to.be.ok;
-        expect(vm.$el.querySelector('.kd-drawer-main').style.right).to.equal('0px')
         expect(vm.$el.querySelector('.kd-drawer-main').style.height).to.equal('')
         expect(vm.$el.querySelector('.kd-drawer-main').style.width).to.equal('480px')
     });
@@ -33,7 +32,6 @@ describe('drawer', () => {
                 value: true,
                 position: 'bottom'
             })
-            expect(vm.$el.querySelector('.kd-drawer-main').style.bottom).to.equal('0px')
             expect(vm.$el.querySelector('.kd-drawer-main').style.height).to.equal('300px')
             expect(vm.$el.querySelector('.kd-drawer-main').style.width).to.equal('100%')
         })
